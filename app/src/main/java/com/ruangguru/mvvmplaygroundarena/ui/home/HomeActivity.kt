80222132dc7b4dd41fc6dff93db9f1d44e268520
nicolascatur <nikolascatur@gmail.com>
2023-01-13 04:17:00 +0700

    fix: move gradle setting in one file

---
+++ app/src/main/java/com/ruangguru/mvvmplaygroundarena/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.ruangguru.mvvmplaygroundarena.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ruangguru.mvvmplaygroundarena.R
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
